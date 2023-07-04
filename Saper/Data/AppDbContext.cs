1a98ddba643cbffa43be77deaf583b04a10c9b4d
Antonio Dilone <antoniodilone26@gmail.com>
2023-07-04 16:53:21 -0400

    Agregar archivos de proyecto.

---
+++ Saper/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Saper.Models;
+
+namespace Saper.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ Saper/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿namespace Saper.Models
+{
+    public class Usuario
+    {
+        public string Id { get; set; }
+
+        public string PrimerNombre { get; set; }
+
+        public string SegundoNombre { get; set;}
+
+        public string telefono { get; set; }
+
+        public string email { get; set; }
+    }
+}
